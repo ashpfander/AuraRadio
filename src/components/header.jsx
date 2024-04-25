cacd95a7c4130e6e0a8bf06c7eca123f00e367bb
leadencat <jdanders1999@gmail.com>
2024-04-25 19:03:15 -0400

    added package script and basic styling, organzied some folders and added new front end folders

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+// Here we are importing a CSS file as a dependency
+import '../styles/header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Home</h1>
+    </header>
+  );
+}
+
+export default Header;
