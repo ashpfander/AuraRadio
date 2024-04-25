b14ec5702e1cd5b7b854efce476754039262de00
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-04-25 19:21:01 -0400

    added and updated all package.jsons and got the application to run

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/aura-radio', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
+      // useFindAndModify: false,
+      // useCreateIndex: true
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
