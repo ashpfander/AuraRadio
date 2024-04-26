9db377920efd0575fdaebb4a7c89bf6d09f6c655
leadencat <jdanders1999@gmail.com>
2024-04-25 20:02:58 -0400

    test #2

--- server/models/mood.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const moodSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String
-});
-
-module.exports = mongoose.model('Mood', moodSchema);
