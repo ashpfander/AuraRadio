9bdfb37054d546e91c890bf3fba836b039e7c0d1
leadencat <jdanders1999@gmail.com>
2024-04-29 19:08:19 -0400

    added moods cards for homepage

--- client/src/pages/Moods.jsx
+++ client/src/pages/Moods.jsx
@@ -1,9 +1,49 @@
+
+
+const moods = [
+  {
+    title: "Happy",
+    description: "."
+  },
+  {
+    title: "Sad",
+    description: "."
+  },
+  {
+    title: "Energized",
+    description: ".",
+  },
+  {
+    title: "Nolstagic",
+    description: "."
+  },
+  {
+    title: "Rock",
+    description: "."
+  },
+  {
+    title: "Metal",
+    description: "."
+  },
+  {
+    title: "Grunge",
+    description: "."
+  },
+  {
+    title: "Pop",
+    description: "."
+  }
+];
+
+
 function Moods() {
   return (
-    <div>
-      
+    <div className="card">
+     <div className="Moods text center p-5">
+      <h2>Select a Playlist for your mood!</h2>
+    </div>
     </div>
   );
 }
 
-export default Moods
+export default Moods;
