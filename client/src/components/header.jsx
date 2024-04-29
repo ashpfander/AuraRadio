0989546ceef7be97552874c33e6dae1e98340f28
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-04-29 19:28:54 -0400

    fixed background color only changing to white randomly

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <header className="ps-4">
       <nav className="navbar navbar-expand-lg navbar-dark">
         <div className="container-fluid d-flex align-items-center">
-          <h1>Aura Radio ၊၊||၊၊၊||၊၊၊||၊</h1>
+          <a href="/" className="home-link"><h1>Aura Radio ၊၊||၊၊၊||၊၊၊||၊</h1></a>
           <Navbar />
         </div>
       </nav>
