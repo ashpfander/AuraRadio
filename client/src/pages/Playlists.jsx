cb12463bd8aefe7345477f255fdb8dddf419ea4d
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-05-02 16:37:00 -0400

    finally got the current selected mood to display on playlist page

--- client/src/pages/Playlists.jsx
+++ client/src/pages/Playlists.jsx
@@ -1,16 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useMutation, useQuery } from '@apollo/client';
+import { useMutation, useQuery, useApolloClient } from '@apollo/client';
 import { CREATE_PLAYLIST } from '../utils/mutations';
-import { GET_PLAYLISTS_BY_MOOD } from '../utils/queries';
+import { GET_PLAYLISTS_BY_MOOD, GET_MOODS } from '../utils/queries';
 import AuthService from '../utils/auth';
 
 function Playlists() {
   const [title, setTitle] = useState('');
   const [iframeInput, setIframeInput] = useState('');
   const [description, setDescription] = useState('');
-  const { moodId } = useParams();
+  const { moodId } = useParams()
   const userId = AuthService.getUserId();  
+  const [moodName, setMoodName] = useState('');
+  const client = useApolloClient();
+
+  const { loading: queryLoading, error: queryError, data } = useQuery(GET_PLAYLISTS_BY_MOOD, {
+    variables: { moodId }
+  });
+  
+  useEffect(() => {
+    const getMoodName = async () => {
+      try {
+        const { data } = await client.query({
+          query: GET_MOODS,
+          variables: { moodId }
+        });
+
+        if (data && data.getMoods) {
+          const currentMood = data.getMoods.find(mood => mood.id === moodId);
+          if (currentMood) {
+            const currentMoodName = currentMood.name;
+            // console.log(currentMoodName);
+            setMoodName(currentMoodName);
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching mood name:', error);
+      }
+    };
+
+    getMoodName();
+  }, [moodId]);
 
   const [createPlaylist, { loading: mutationLoading, error: mutationError }] = useMutation(CREATE_PLAYLIST, {
     onCompleted: data => {
@@ -38,10 +68,6 @@ function Playlists() {
       if (error.graphQLErrors) console.error('GraphQL Errors:', error.graphQLErrors);
       if (error.networkError) console.error('Network Error:', error.networkError);
     }
-  });
-
-  const { loading: queryLoading, error: queryError, data } = useQuery(GET_PLAYLISTS_BY_MOOD, {
-    variables: { moodId }
   });
 
   const handleSubmit = async (event) => {
@@ -75,51 +101,53 @@ function Playlists() {
   if (queryError) return <p>Error loading playlists: {queryError.message}</p>;
 
   return (
-    <div className="container mt-5 d-flex justify-content-center">
-      <div className="row">
-      <h2 className="text-center">Drop a Vibe</h2>
-      <form onSubmit={handleSubmit} className="col-12">
-        <input
-          type="text"
-          className="form-control mb-3"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Name It"
-          required
-        />
-        <textarea
-          className="form-control mb-3"
-          value={iframeInput}
-          onChange={(e) => setIframeInput(e.target.value)}
-          placeholder="Paste the Magic here"
-          required
-          style={{ width: '100%', height: '100px' }}
-        />
-        <textarea
-          className="form-control mb-3"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Paint the picture"
-          required
-          style={{ width: '100%', height: '50px' }}
-        />
-        <button type="submit" className="form-button p-3 mb-4" disabled={mutationLoading}>
-        Send It to the Clouds
-        </button>
-        {mutationLoading && <p>Submitting...</p>}
-        {mutationError && <p>Error submitting playlist: {mutationError.message}</p>}
-      </form>
-      <div>
-        <h3 className="text-center">Vibe Vault</h3>
-        {data && data.getPlaylistsByMood.map((playlist, index) => (
-          <div key={index}>
-            <hr className="line"/>
-            <h4>{playlist.title}</h4>
-            <p>{playlist.description}</p>
-            <div dangerouslySetInnerHTML={{ __html: playlist.iframeContent }} />
+    <div className="container">
+      <div className="mt-5 d-flex justify-content-center">
+        <div className="row">
+          <h2 className="text-center">Drop a Vibe for {moodName}</h2>
+          <form onSubmit={handleSubmit} className="col-12">
+            <input
+            type="text"
+            className="form-control mb-3"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Name It"
+            required
+            />
+            <textarea
+            className="form-control mb-3"
+            value={iframeInput}
+            onChange={(e) => setIframeInput(e.target.value)}
+            placeholder="Paste the Magic here"
+            required
+            style={{ width: '100%', height: '100px' }}
+            />
+            <textarea
+            className="form-control mb-3"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Paint the picture"
+            required
+            style={{ width: '100%', height: '50px' }}
+            />
+            <button type="submit" className="form-button p-3 mb-4" disabled={mutationLoading}>
+            Send It to the Clouds
+            </button>
+            {mutationLoading && <p>Submitting...</p>}
+            {mutationError && <p>Error submitting playlist: {mutationError.message}</p>}
+          </form>
+          <div>
+            <h3 className="text-center">Vibe Vault</h3>
+            {data && data.getPlaylistsByMood.map((playlist, index) => (
+            <div key={index}>
+              <hr className="line"/>
+              <h4>{playlist.title}</h4>
+              <p>{playlist.description}</p>
+              <div dangerouslySetInnerHTML={{ __html: playlist.iframeContent }} />
+            </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
       </div>
     </div>
   );
